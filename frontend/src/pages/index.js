9f124febb1839da34d27b0fe9c8450e913fa764d
zepvalue <zepvalue@gmail.com>
2021-12-06 13:42:39 +0100

    added fe pagination and some css

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,71 +1,73 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import axios from "axios";
 import DataTable from "react-data-table-component";
 import DatePicker from "react-datepicker";
-import { format, compareAsc } from "date-fns";
+import { format } from "date-fns";
 import "react-datepicker/dist/react-datepicker.css";
 import "react-datepicker/dist/react-datepicker-cssmodules.min.css";
-import { StaticQuery } from "gatsby";
 
 // styles
 const pageStyles = {
   color: "#232129",
+  display: "flex",
+  flexDirection: "column",
+  border: "1px blue solid",
   padding: 96,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
 };
 const headingStyles = {
   marginTop: 0,
   marginBottom: 64,
-  maxWidth: 320,
+  maxWidth: 1000,
 };
-
-const columns = [
-  {
-    id: 1,
-    name: "visit_id",
-    selector: (row) => row.visit_id,
-    sortable: true,
-    reorder: true,
-  },
-  {
-    id: 2,
-    name: "ip_address",
-    selector: (row) => row.ip_address,
-    sortable: true,
-    reorder: true,
-  },
-  {
-    id: 3,
-    name: "last_access",
-    selector: (row) => row.last_access,
-    sortable: true,
-    right: true,
-    reorder: true,
-  },
-];
 
 const IndexPage = () => {
   const [ip, setIp] = useState("");
-  const [visits, setVisits] = useState({});
+  const [visits, setVisits] = useState([]);
   const today = new Date();
   const [startDate, setStartDate] = useState(today);
+  const [loading, setLoading] = useState(false);
 
-  const fetchFilteredRes = (date) => {
+  const columns = useMemo(() => [
+    {
+      id: 1,
+      name: "visit_id",
+      selector: (row) => row.visit_id,
+      sortable: true,
+      reorder: true,
+    },
+    {
+      id: 2,
+      name: "ip_address",
+      selector: (row) => row.ip_address,
+      sortable: true,
+      reorder: true,
+    },
+    {
+      id: 3,
+      name: "last_access",
+      selector: (row) => row.last_access,
+      sortable: true,
+      right: true,
+      reorder: true,
+    },
+  ]);
+
+  const fetchFilteredRes = async (date) => {
     const formattedDate = format(date, "MM-dd-yyyy");
-    console.log(
-      "🚀 ~ file: index.js ~ line 55 ~ fetchFilteredRes ~ formattedDate",
-      formattedDate
+    setLoading(true);
+
+    const res = await axios.get(
+      `http://127.0.0.1:5000/api/visits/show?date=${formattedDate}`
     );
 
-    axios
-      .get(`http://127.0.0.1:5000/api/visits/show?date=${formattedDate}`)
-      .then((res) => {
-        console.log("CALLED FROM THE CALENDAR ", res);
-        setVisits(res.data);
-      });
+    setVisits(res.data);
+    setLoading(false);
   };
 
   useEffect(() => {
+    setLoading(true);
+
     axios.get("https://api.db-ip.com/v2/free/self").then((response) => {
       setIp(response.data.ipAddress);
 
@@ -76,34 +78,54 @@ const IndexPage = () => {
     });
 
     axios.get("http://127.0.0.1:5000/api/visits/show").then((res) => {
+      setLoading(true);
       setVisits(res.data);
+      setLoading(false);
     });
   }, []);
 
   return (
     <main style={pageStyles}>
-      <title>Home Page</title>
-      <span>
+      <title>Visits Tracker</title>
+      <div>
         <h1 style={headingStyles}>{`Today is : ${format(
           startDate,
           "EEEE,MMMM do, yyyy hh:mm a"
         )}`}</h1>
-      </span>
+      </div>
 
-      <div>
-        <p>Pick a date to filter the table</p>
-        <DatePicker
-          selected={startDate}
-          onChange={(date) => {
-            setStartDate(date);
-            fetchFilteredRes(date);
-          }}
-        />
+      <div
+        style={{
+          border: "1px red dotted",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <div>
+          <p>Pick a date to filter the table: </p>
+        </div>
+        <div style={{ padding: "10px" }}>
+          <DatePicker
+            selected={startDate}
+            onChange={(date) => {
+              setStartDate(date);
+              fetchFilteredRes(date);
+            }}
+          />
+        </div>
       </div>
 
       <div> {`Your ip address: ${ip}`}</div>
       <div>
-        <DataTable title="Visits" columns={columns} data={visits} />
+        <DataTable
+          title="Visits"
+          columns={columns}
+          data={visits}
+          progressPending={loading}
+          pagination
+          paginationPerPage={5}
+          paginationRowsPerPageOptions={[5, 15, 25, 50]}
+        />
       </div>
     </main>
   );
